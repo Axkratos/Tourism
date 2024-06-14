9547d8f4b67b45c5fd4edb3af7c6595cb124d252
jyotiraut <jyoti.211314@ncit.edu.np>
2024-06-15 00:48:51 +0545

    error

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,10 +21,13 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
+app.use(cors(corsOptions))
 
 // Routes
 app.use("/api/user", userRoutes);
