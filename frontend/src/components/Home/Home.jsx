d4dfe73ce0c62d0bdb7c4a32daf6039e2e1add9b
Mamta Sharma <mamta.211315@ncit.edu.np>
2024-06-14 20:17:54 +0630

    adding hero

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Appreview from '../homecomponents/Appreview.jsx'
 import Topuser from '../homecomponents/Topuser'
+import Widget from '../dashboard/demo.jsx'
 
 const Home = () => {
   return (
     <div>
+        <Widget/>
         <Appreview/>
         <Topuser/>
       
