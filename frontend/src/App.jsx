785ec27bdc9dbc6b6c4d1b728fe74f5db151c863
Mamta Sharma <mamta.211315@ncit.edu.np>
2024-06-14 19:50:54 +0630

    top rated user and appreview ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Navbar } from './components/common/Navbar'
 import Footer from './components/common/Footer'
+import Home from './components/Home/Home'
 // import Navbar from './components/common/Navbar.jsx'
 // import Footer from './components/common/Footer.jsx'
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Home/>
       <Footer/>
     </>
   )
---
+++ frontend/src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Appreview from '../homecomponents/Appreview.jsx'
+import Topuser from '../homecomponents/Topuser'
+
+const Home = () => {
+  return (
+    <div>
+        <Appreview/>
+        <Topuser/>
+      
+    </div>
+  )
+}
+
+export default Home
