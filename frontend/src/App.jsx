49b83601202624f3b76946398e5a5f9633940b4c
Avash Mani Dahal <avash.211305@ncit.edu.np>
2024-06-14 19:59:06 +0545

    styled navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import React from 'react'
 import { Navbar } from './components/common/Navbar'
 import Footer from './components/common/Footer'
+import Home from './components/Home/Home'
+import Dash from './components/dashboard/dash'
+
 // import Navbar from './components/common/Navbar.jsx'
 // import Footer from './components/common/Footer.jsx'
 
