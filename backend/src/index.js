b9851adb4bd08cbc6f52c6ca6f1daed36abcd6c2
Mamta Sharma <mamta.211315@ncit.edu.np>
2024-06-15 10:27:50 +0630

    nothing

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173', // Specify your frontend URL
+  origin: 'http://localhost:5175', // Specify your frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
